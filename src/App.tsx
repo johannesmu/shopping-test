a903cb530f1b12b80387aeaa2e01b893251eb47e
johannes <jmuljana@gmail.com>
2026-02-23 17:51:48 +1100

    installed router

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from 'react-router'
+import './App.css'
+import { Home } from "../views/Home"
+import { About } from "../views/About"
+
+function App() {
+  
+
+  return (
+    <>
+     <Routes>
+        <Route path='/' element={ <Home/> }/>
+        <Route path='/about' element={ <About/> } />
+     </Routes>
+    </>
+  )
+}
+
+export default App
